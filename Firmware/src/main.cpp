eff4e92f02bcb42fb5c0f7fd1ed8f9dee654422e
Mark Sherstan <mshersta@ualberta.ca>
2021-11-25 12:23:08 -0700

    Initial testing script

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define switchPin   3
+#define statusLED   2
+
+// Functions
+void sleep();
+
+
+void setup()
+{
+    pinMode(switchPin, INPUT);
+    digitalWrite(switchPin, HIGH);
+    pinMode(statusLED, OUTPUT);
+
+    // Flash quick sequence so we know setup has started
+    for (int k = 0; k < 10; k = k + 1)
+    {
+        if (k % 2 == 0)
+        {
+            digitalWrite(statusLED, HIGH);
+        }
+        else
+        {
+            digitalWrite(statusLED, LOW);
+        }
+        delay(250);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    sleep();
+    digitalWrite(statusLED, HIGH);
+    delay(1000);
+    digitalWrite(statusLED, LOW);
 }
+
+void sleep()
+{
+
+    GIMSK |= _BV(PCIE);                  // Enable Pin Change Interrupts
+    PCMSK |= _BV(PCINT3);                // Use PB3 as interrupt pin
+    ADCSRA &= ~_BV(ADEN);                // ADC off
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); // replaces above statement
+
+    sleep_enable(); // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+    sei();          // Enable interrupts
+    sleep_cpu();    // sleep
+
+    cli();                 // Disable interrupts
+    PCMSK &= ~_BV(PCINT3); // Turn off PB3 as interrupt pin
+    sleep_disable();       // Clear SE bit
+    ADCSRA |= _BV(ADEN);   // ADC on
+
+    sei(); // Enable interrupts
+}
+
+ISR(PCINT0_vect){}
