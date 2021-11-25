c9bde7a30f35d485a9071c0a7518ae155ad1d335
Mark Sherstan <mshersta@ualberta.ca>
2021-11-25 12:41:25 -0700

    Charlieplexing temp holder

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -9,6 +9,44 @@
 void sleep();
 
 
+#define A 8
+#define B 9
+#define C 10
+#define D 11
+
+#define PIN_CONFIG 0
+#define PIN_STATE 1
+
+#define LED_Num 12
+
+int matrix[LED_Num][2][4] = {
+  //           PIN_CONFIG                  PIN_STATE
+  //    A       B       C      D         A     B    C    D
+  { { OUTPUT, OUTPUT, INPUT, INPUT }, { HIGH, LOW, LOW, LOW } }, 
+  { { OUTPUT, OUTPUT, INPUT, INPUT }, { LOW, HIGH, LOW, LOW } }, 
+  { { INPUT, OUTPUT, OUTPUT, INPUT }, { LOW, HIGH, LOW, LOW } },
+  { { INPUT, OUTPUT, OUTPUT, INPUT }, { LOW, LOW, HIGH, LOW } }, 
+  { { OUTPUT, INPUT, OUTPUT, INPUT }, { HIGH, LOW, LOW, LOW } },
+  { { OUTPUT, INPUT, OUTPUT, INPUT }, { LOW, LOW, HIGH, LOW } }, 
+  { { OUTPUT, INPUT, INPUT, OUTPUT }, { HIGH, LOW, LOW, LOW } }, 
+  { { OUTPUT, INPUT, INPUT, OUTPUT }, { LOW, LOW, LOW, HIGH } }, 
+  { { INPUT, OUTPUT, INPUT, OUTPUT }, { LOW, HIGH, LOW, LOW } }, 
+  { { INPUT, OUTPUT, INPUT, OUTPUT }, { LOW, LOW, LOW, HIGH } }, 
+  { { INPUT, INPUT, OUTPUT, OUTPUT }, { LOW, LOW, HIGH, LOW } }, 
+  { { INPUT, INPUT, OUTPUT, OUTPUT }, { LOW, LOW, LOW, HIGH } }  
+};
+
+void lightOn( int led ) {
+  pinMode( A, matrix[led][PIN_CONFIG][0] );
+  pinMode( B, matrix[led][PIN_CONFIG][1] );
+  pinMode( C, matrix[led][PIN_CONFIG][2] );
+  pinMode( D, matrix[led][PIN_CONFIG][3] );
+  digitalWrite( A, matrix[led][PIN_STATE][0] );
+  digitalWrite( B, matrix[led][PIN_STATE][1] );
+  digitalWrite( C, matrix[led][PIN_STATE][2] );
+  digitalWrite( D, matrix[led][PIN_STATE][3] );
+}
+
 void setup()
 {
     pinMode(switchPin, INPUT);
@@ -40,7 +78,6 @@ void loop()
 
 void sleep()
 {
-
     GIMSK |= _BV(PCIE);                  // Enable Pin Change Interrupts
     PCMSK |= _BV(PCINT3);                // Use PB3 as interrupt pin
     ADCSRA &= ~_BV(ADEN);                // ADC off
@@ -59,3 +96,5 @@ void sleep()
 }
 
 ISR(PCINT0_vect){}
+
+
